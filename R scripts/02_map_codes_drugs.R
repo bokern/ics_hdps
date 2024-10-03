5fcbfe214626a286e52d5113d01e99e8fc501391
M. Bokern <103196673+bokern@users.noreply.github.com>
2024-10-03 10:30:21 +0100

    variable name correction

--- R scripts/02_map_codes_drugs.R
+++ R scripts/02_map_codes_drugs.R
@@ -2,10 +2,17 @@
 # Project: HDPS
 # Programmer name: Marleen Bokern
 # Date Started:  06/24
-#############################  
+#############################
 # Map prodcodes to BNF paragraphs
 #############################
-packages <- c("tidyverse", "MetBrewer", "arrow", "readstata13", "data.table", "autoCovariateSelection")
+packages <- c(
+  "tidyverse",
+  "MetBrewer",
+  "arrow",
+  "readstata13",
+  "data.table",
+  "autoCovariateSelection"
+)
 installed_packages <- packages %in% rownames(installed.packages())
 if (any(installed_packages == FALSE)) {
   install.packages(packages[!installed_packages])
@@ -21,8 +28,15 @@ palette <- met.brewer("Cassatt2")
 setwd(Copd_aurum_extract)
 
 #code_browser as string variables
-file_path <- file.path(Denominator, "CPRD Aurum", "Code browsers", "2022_05" , "CPRDAurumProduct.txt")
-code_browser <- read.delim(file_path, sep = "\t", header = TRUE, colClasses = "character") %>% dplyr::select(ProdCodeId, dmdid, BNFChapter, Term.from.EMIS, DrugIssues)
+file_path <- file.path(Denominator,
+                       "CPRD Aurum",
+                       "Code browsers",
+                       "2022_05" ,
+                       "CPRDAurumProduct.txt")
+code_browser <- read.delim(file_path,
+                           sep = "\t",
+                           header = TRUE,
+                           colClasses = "character") %>% dplyr::select(ProdCodeId, dmdid, BNFChapter, Term.from.EMIS, DrugIssues)
 # Convert DrugIssues to numeric and format without scientific notation
 code_browser$DrugIssues <- format(as.numeric(code_browser$DrugIssues), scientific = FALSE)
 
@@ -31,41 +45,87 @@ file_path <- file.path(HDPS_folder, "bnf_to_dmd.csv")
 bnf_snomed <- read.csv(file_path, header = TRUE, colClasses = "character") %>% dplyr::select(-X, -vmp_previous, -vmp_previous_date, -type, -vtm, -nm, -vtm_nm)
 
 #left join code_browser with bnf_snomed
-code_map <- code_browser %>% 
+code_map <- code_browser %>%
   left_join(bnf_snomed, by = c("dmdid" = "id"))
 
 #create bnf_paragraph
 code_map$bnf_paragraph <- case_when(
-  !is.na(code_map$bnf_code) & nchar(code_map$bnf_code) >= 6 ~ substr(code_map$bnf_code, 1, 6),
-  !is.na(code_map$BNFChapter) & nchar(code_map$BNFChapter) == 7 ~ str_pad(substr(code_map$BNFChapter, 1, 5), 6, side = "left", pad = "0"),
-  !is.na(code_map$BNFChapter) & nchar(code_map$BNFChapter) == 8 ~ substr(code_map$BNFChapter, 1, 6),
+  !is.na(code_map$bnf_code) &
+    nchar(code_map$bnf_code) >= 6 ~ substr(code_map$bnf_code, 1, 6),!is.na(code_map$BNFChapter) &
+    nchar(code_map$BNFChapter) == 7 ~ str_pad(
+      substr(code_map$BNFChapter, 1, 5),
+      6,
+      side = "left",
+      pad = "0"
+    ),!is.na(code_map$BNFChapter) &
+    nchar(code_map$BNFChapter) == 8 ~ substr(code_map$BNFChapter, 1, 6),
   TRUE ~ NA_character_
 )
 
 #pick out codes with bnf_paragraph = NA and sort on DrugIssues
-missing <- code_map %>% 
-  filter(is.na(bnf_paragraph)) %>% 
-  arrange(desc(DrugIssues)) %>% 
+missing <- code_map %>%
+  filter(is.na(bnf_paragraph)) %>%
+  arrange(desc(DrugIssues)) %>%
   dplyr::select(ProdCodeId, dmdid, BNFChapter, Term.from.EMIS, DrugIssues)
 
 # Map specific terms to BNF paragraphs when bnf_paragraph is empty
 code_map$bnf_paragraph <- case_when(
-  is.na(code_map$bnf_paragraph) & grepl("paracetamol.*mg.*", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "040701",
-  is.na(code_map$bnf_paragraph) & grepl("mirtazapine.*tablets", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "040304",
-  is.na(code_map$bnf_paragraph) & grepl("apixaban.*tablets.*", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "020802",
-  is.na(code_map$bnf_paragraph) & grepl("aspirin.*mg", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "020900",
-  is.na(code_map$bnf_paragraph) & grepl("rosuvastatin.*tablets", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "021200",
-  is.na(code_map$bnf_paragraph) & grepl("candesartan.*tablets", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "020505",
-  is.na(code_map$bnf_paragraph) & grepl("quinine bisulfate", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "050401",
-  is.na(code_map$bnf_paragraph) & grepl("epimax.*cream", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "212200",
-  is.na(code_map$bnf_paragraph) & grepl("sitagliptine.*tablets", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "060102",
-  is.na(code_map$bnf_paragraph) & grepl("ventolin", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "030101",
-  is.na(code_map$bnf_paragraph) & grepl("amlodipin.*tablets.*mg", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "020602",
-  is.na(code_map$bnf_paragraph) & grepl("gaviscon", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "010102",
-  is.na(code_map$bnf_paragraph) & grepl("erythromycin", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "050105",
-  is.na(code_map$bnf_paragraph) & grepl("fluticasone.*nasal spray", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "120201",
-  is.na(code_map$bnf_paragraph) & grepl("salbutamol aerosol", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "030101",
-  is.na(code_map$bnf_paragraph) & grepl("co-codamol", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "040701",
+  is.na(code_map$bnf_paragraph) &
+    grepl("paracetamol.*mg.*", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "040701",
+  is.na(code_map$bnf_paragraph) &
+    grepl(
+      "mirtazapine.*tablets",
+      code_map$Term.from.EMIS,
+      ignore.case = TRUE
+    ) ~ "040304",
+  is.na(code_map$bnf_paragraph) &
+    grepl("apixaban.*tablets.*", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "020802",
+  is.na(code_map$bnf_paragraph) &
+    grepl("aspirin.*mg", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "020900",
+  is.na(code_map$bnf_paragraph) &
+    grepl(
+      "rosuvastatin.*tablets",
+      code_map$Term.from.EMIS,
+      ignore.case = TRUE
+    ) ~ "021200",
+  is.na(code_map$bnf_paragraph) &
+    grepl(
+      "candesartan.*tablets",
+      code_map$Term.from.EMIS,
+      ignore.case = TRUE
+    ) ~ "020505",
+  is.na(code_map$bnf_paragraph) &
+    grepl("quinine bisulfate", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "050401",
+  is.na(code_map$bnf_paragraph) &
+    grepl("epimax.*cream", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "212200",
+  is.na(code_map$bnf_paragraph) &
+    grepl(
+      "sitagliptine.*tablets",
+      code_map$Term.from.EMIS,
+      ignore.case = TRUE
+    ) ~ "060102",
+  is.na(code_map$bnf_paragraph) &
+    grepl("ventolin", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "030101",
+  is.na(code_map$bnf_paragraph) &
+    grepl(
+      "amlodipin.*tablets.*mg",
+      code_map$Term.from.EMIS,
+      ignore.case = TRUE
+    ) ~ "020602",
+  is.na(code_map$bnf_paragraph) &
+    grepl("gaviscon", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "010102",
+  is.na(code_map$bnf_paragraph) &
+    grepl("erythromycin", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "050105",
+  is.na(code_map$bnf_paragraph) &
+    grepl(
+      "fluticasone.*nasal spray",
+      code_map$Term.from.EMIS,
+      ignore.case = TRUE
+    ) ~ "120201",
+  is.na(code_map$bnf_paragraph) &
+    grepl("salbutamol aerosol", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "030101",
+  is.na(code_map$bnf_paragraph) &
+    grepl("co-codamol", code_map$Term.from.EMIS, ignore.case = TRUE) ~ "040701",
   TRUE ~ code_map$bnf_paragraph
 )
 
@@ -77,18 +137,22 @@ code_map$bnf_paragraph <- case_when(
 )
 
 #save codes that remain unmatched to drugs_counts_unmatched.csv
-unmatched <- code_map %>% 
-  filter(is.na(bnf_paragraph)) %>% 
-  dplyr::select(ProdCodeId, dmdid, BNFChapter, Term.from.EMIS, DrugIssues) %>% 
+unmatched <- code_map %>%
+  filter(is.na(bnf_paragraph)) %>%
+  dplyr::select(ProdCodeId, dmdid, BNFChapter, Term.from.EMIS, DrugIssues) %>%
   #select the top 100 unmatched codes by DrugIssues
   arrange(desc(DrugIssues)) %>%
   head(100)
-  
-#save to file
-write.csv(unmatched, file = file.path(HDPS_folder, "drugs_counts_unmatched.csv"), row.names = FALSE)
 
-code_map_exp <- code_map %>%  filter(!is.na(bnf_paragraph)) %>% 
-  dplyr::select(ProdCodeId, bnf_paragraph) %>% 
+#save to file
+write.csv(
+  unmatched,
+  file = file.path(HDPS_folder, "drugs_counts_unmatched.csv"),
+  row.names = FALSE
+)
+
+code_map_exp <- code_map %>%  filter(!is.na(bnf_paragraph)) %>%
+  dplyr::select(ProdCodeId, bnf_paragraph) %>%
   distinct()
 
 # Ensure ProdCodeId is character and format without scientific notation
@@ -104,11 +168,15 @@ file_path <- file.path(HDPS_folder, "map_prodcode_bnf.txt")
 code_map_exp$ProdCodeId <- gsub(" ", "", code_map_exp$ProdCodeId)
 
 # Write the data frame to txt, ensuring no scientific notation and all columns as text
-write.table(code_map_exp, file = file_path, row.names = FALSE, quote = TRUE, 
-            fileEncoding = "UTF-8")
+write.table(
+  code_map_exp,
+  file = file_path,
+  row.names = FALSE,
+  quote = TRUE,
+  fileEncoding = "UTF-8"
+)
 
 #remove ICS, LABA and LAMA from drugs_all
-#load text codelists for the 3 drug classes 
 # Function to read the first part of each line from a text file
 read_first_part_of_lines <- function(filepath) {
   lines <- readLines(filepath)
@@ -128,18 +196,26 @@ ics_laba_codes <- read_first_part_of_lines(file.path(Github_codelists, "cl_ics_l
 triple_therapy_codes <- read_first_part_of_lines(file.path(Github_codelists, "cl_triple_therapy.txt"))
 
 # Combine all codes into one vector
-all_codes <- c(ics_single_codes, laba_single_codes, lama_single_codes, laba_lama_codes, ics_laba_codes, triple_therapy_codes)
+all_codes <- c(
+  ics_single_codes,
+  laba_single_codes,
+  lama_single_codes,
+  laba_lama_codes,
+  ics_laba_codes,
+  triple_therapy_codes
+)
 
 setwd(Copd_aurum_extract)
 # List of drug files to process
-drug_files <- list.files(Copd_aurum_extract, pattern = "Drugs_for_hdps_.*\\.parquet", full.names = TRUE)
+drug_files <- list.files(Copd_aurum_extract,
+                         pattern = "Drugs_for_hdps_.*\\.parquet",
+                         full.names = TRUE)
 
 # Initialize an empty dataframe to store all drugs_empty_bnf
 all_drugs_empty_bnf <- data.frame()
 drugs_all <- data.frame()
 
 for (file in drug_files) {
-
   # Extract file number for naming
   file_num <- str_extract(basename(file), "\\d+")
   
@@ -149,42 +225,18 @@ for (file in drug_files) {
   #remove ICS, LABA and LAMA from drugs_all
   drugs <- drugs[!(prodcodeid %in% all_codes)]
   # Merge with code_browser
-  drugs <- drugs %>% 
-    left_join(code_map, by = c("prodcodeid" = "ProdCodeId")) %>% 
-    dplyr::select(patid, bnf_paragraph, issuedate) %>% 
+  drugs <- drugs %>%
+    left_join(code_map, by = c("prodcodeid" = "ProdCodeId")) %>%
+    dplyr::select(patid, bnf_paragraph, issuedate) %>%
     #rename BNFchapter to code
     rename(code = bnf_paragraph)
-
-  # # Count number of empty values in dmdid and BNFChapter
-  # print(paste("File", file_num, "- Empty dmdid:", sum(drugs$dmdid == "")))
-  # print(paste("File", file_num, "- Empty BNFChapter:", sum(drugs$BNFChapter == "")))
-  # # Merge with drugs on dmdid = SNOMED Code, keep BNF Code, BNF Name
-  # drugs <- drugs %>%
-  #   left_join(bnf_snomed, by = c("dmdid" = "id")) %>%
-  #   dplyr::select(BNFChapter, bnf_code, everything())
-  # 
-  # # Count if BNF Chapter and BNF Code are empty
-  # print(paste("File", file_num, "- Empty BNFChapter and bnf_code:", sum(drugs$BNFChapter == "" & is.na(drugs$bnf_code))))
-  # print(paste("File", file_num, "- Empty bnf_code:", sum(is.na(drugs$bnf_code))))
-  # print(paste("File", file_num, "- Empty BNFChapter:", sum(drugs$BNFChapter == "")))
-  # 
-  # # Create bnf_paragraph
-  # drugs$bnf_paragraph <- case_when(
-  #   !is.na(drugs$bnf_code) & nchar(drugs$bnf_code) >= 6 ~ substr(drugs$bnf_code, 1, 6),
-  #   !is.na(drugs$BNFChapter) & nchar(drugs$BNFChapter) == 7 ~ str_pad(substr(drugs$BNFChapter, 1, 5), 6, side = "left", pad = "0"),
-  #   !is.na(drugs$BNFChapter) & nchar(drugs$BNFChapter) == 8 ~ substr(drugs$BNFChapter, 1, 6),
-  #   TRUE ~ NA_character_
-  # )
-  # 
-  # # Count if bnf_paragraph is empty
-  # print(paste("File", file_num, "- Empty bnf_paragraph:", sum(is.na(drugs$bnf_paragraph))))
   
   # Save cut of data with empty BNF Chapter and BNF Code
   drugs_empty_bnf <- drugs %>% filter(is.na(drugs$code))
   
   # Append drugs_empty_bnf to all_drugs_empty_bnf
   all_drugs_empty_bnf <- rbind(all_drugs_empty_bnf, drugs_empty_bnf)
- 
+  
   #remove rows with no bnf_paragraph from drugs_all
   drugs <- drugs %>% filter(!is.na(code))
   drugs_all <- rbind(drugs_all, drugs)
@@ -194,9 +246,8 @@ for (file in drug_files) {
 write_parquet(all_drugs_empty_bnf, "all_drugs_empty_bnf.parquet")
 
 #drop bnf_code and BNFChapter from drugs_all
-#drugs_all <- drugs_all %>% dplyr::select(c(patid, bnf_paragraph, issuedate))
+drugs_all <- drugs_all %>% dplyr::select(c(patid, code, issuedate))
 
 setwd(Datadir_copd)
 
 write_parquet(drugs_all, "drugs_for_hdps_mapped.parquet")
-
