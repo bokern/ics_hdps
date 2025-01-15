faf99afca3e732f97a77f5553c12d808913cb289
M. Bokern <103196673+bokern@users.noreply.github.com>
2025-01-15 08:52:11 +0000

    update

--- R scripts/hdps-diagnostics/chapters_prevalence.R
+++ R scripts/hdps-diagnostics/chapters_prevalence.R
@@ -1,7 +1,7 @@
 ###########################################################
 # R script:    chapters_prevalence.R
 #
-# Author:      Marleen Bokern
+# Author:     John Tazare, Marleen Bokern
 #
 # Date:        Sep 2024
 #
@@ -32,8 +32,7 @@ if (exclude_triple == TRUE) {
 }
 
 # Set working directory (you need to define HDPS_folder)
-# HDPS_folder <- "/path/to/your/HDPS/folder"
-# setwd(HDPS_folder)
+ setwd(HDPS_folder)
 
 # Set color palette
 palette <- met.brewer("Cassatt2")
@@ -43,11 +42,11 @@ topVars <- c("100", "250", "500", "750", "1000")
 outcomes <- c("covid_hes_present", "covid_death_present")
 cohort_exts <- c("", "_no_triple")
 
- for (cohort_ext in cohort_exts) {
-   for (outcome in outcomes) {
-     for (k in topVars) {
-#browser()
-       print(paste0("Processing ", k, " ", outcome, " ", cohort_ext))
+for (cohort_ext in cohort_exts) {
+  for (outcome in outcomes) {
+    for (k in topVars) {
+      
+      print(paste0("Processing ", k, " ", outcome, " ", cohort_ext))
       # Load data from outputs folder
       data <- read.csv(paste0(
         HDPS_folder,
@@ -58,7 +57,7 @@ cohort_exts <- c("", "_no_triple")
         cohort_ext,
         ".csv"
       ))
-   
+      
       # Function to assign chapters to ICD and BNF codes
       assign_chapter <- function(variable) {
         # For ICD codes
@@ -91,6 +90,11 @@ cohort_exts <- c("", "_no_triple")
       data$dim <- as.factor(as.numeric(substr(data$variable, 2, 2)))
       data$chapter <- sapply(substr(data$variable, 4, nchar(data$variable)), assign_chapter)
       
+      # table for dim counts
+      dim_counts <- data %>%
+        group_by(dim) %>%
+        summarise(tot = n(), .groups = 'drop')
+      
       # Create summary dataset
       summary_df <- data %>%
         group_by(dim, chapter) %>%
@@ -102,7 +106,7 @@ cohort_exts <- c("", "_no_triple")
           arrange(desc(tot)) %>%
           slice(1:30)
       }
-
+      
       # Add chapter indicators and descriptions
       chapter_ind <- c(
         "A" = 1,
@@ -162,22 +166,22 @@ cohort_exts <- c("", "_no_triple")
         "3" = "Blood and blood-forming organs diseases",
         "4" = "Endocrine, nutritional and metabolic diseases",
         "5" = "Mental and behavioural disorders",
-        "6" = "Diseases of the nervous system",
-        "7" = "Diseases of the eye and adnexa",
-        "8" = "Diseases of the ear and mastoid process",
-        "9" = "Diseases of the circulatory system",
-        "10" = "Diseases of the respiratory system",
-        "11" = "Diseases of the digestive system",
-        "12" = "Diseases of the skin and subcutaneous tissue",
-        "13" = "Diseases of the musculoskeletal system and connective tissue",
-        "14" = "Diseases of the genitourinary system",
+        "6" = "Nervous system",
+        "7" = "Eye and adnexa",
+        "8" = "Ear and mastoid process",
+        "9" = "Circulatory system",
+        "10" = "Respiratory system",
+        "11" = "Digestive system",
+        "12" = "Skin and subcutaneous tissue",
+        "13" = "Musculoskeletal system, connective tissue",
+        "14" = "Genitourinary system",
         "15" = "Pregnancy, childbirth and the puerperium",
         "16" = "Certain conditions originating in the perinatal period",
-        "17" = "Congenital malformations, deformations and chromosomal abnormalities",
-        "18" = "Symptoms, signs and abnormal findings",
-        "19" = "Injury, poisoning and other external causes",
+        "17" = "Congenital malformations, chromosomal abnormalities",
+        "18" = "Symptoms, signs, abnormal findings",
+        "19" = "Injury, poisoning, other external causes",
         "20" = "Codes for special purposes",
-        "21" = "Health status factors and contact with health services",
+        "21" = "Health status factors, contact with health services",
         "22" = "Codes for special purposes",
         "23" = "Gastrointestinal system",
         "24" = "Cardiovascular system",
@@ -185,8 +189,8 @@ cohort_exts <- c("", "_no_triple")
         "26" = "Central nervous system",
         "27" = "Infections",
         "28" = "Endocrine system",
-        "29" = "Obstetrics, gynaecology and urinary tract disorders",
-        "30" = "Malignant disease and immunosuppression",
+        "29" = "Obstetrics, gynaecology, urinary tract",
+        "30" = "Malignant disease, immunosuppression",
         "31" = "Nutrition and blood",
         "32" = "Musculoskeletal and joint diseases",
         "33" = "Eye",
@@ -227,7 +231,7 @@ cohort_exts <- c("", "_no_triple")
       
       # Add id column for plotting
       summary_df$id <- seq(1, nrow(summary_df))
-  
+      
       # Get the name, angles and position of dimension chapter
       number_of_bar <- nrow(summary_df)
       angle <- 90 - 360 * (summary_df$id - 0.5) / number_of_bar
@@ -237,16 +241,16 @@ cohort_exts <- c("", "_no_triple")
       
       max_prev <- max(summary_df$prev, na.rm = TRUE)
       summary_df <- summary_df %>%
-#        group_by(dim) %>%
+        #        group_by(dim) %>%
         mutate(normalized_prev = tot / sum(tot) * 100) #%>%
-#        ungroup()
+      #        ungroup()
       
       # Set a fixed maximum radius for the plot
-      fixed_max_radius <- 8
+      fixed_max_radius <- 7
       
       # Scale the normalized prevalence to fit within the fixed radius
       summary_df <- summary_df %>%
-      mutate(scaled_prev = (normalized_prev*0.5) / max((normalized_prev), na.rm = TRUE) * fixed_max_radius)
+        mutate(scaled_prev = (normalized_prev*0.5) / max((normalized_prev), na.rm = TRUE) * fixed_max_radius)
       
       # Calculate label positions
       label_data <- summary_df %>%
@@ -285,15 +289,15 @@ cohort_exts <- c("", "_no_triple")
       p <- ggplot(summary_df, aes(x = as.factor(id), y = scaled_prev, fill = dim)) +
         scale_fill_manual(values = colours,
                           labels = c("1" = "Clinical", "2" = "Prescriptions", "3" = "Hospital"),
-                          guide = guide_legend(keywidth = 2, keyheight = 2, size = 24)) +
+                          guide = guide_legend(keywidth = 1.8, keyheight = 1.8, size = 22)) +
         geom_bar(stat = "identity", alpha = 1, width = 0.85) +
-        ylim(-fixed_max_radius * 0.5, fixed_max_radius * 1.2) +  # Adjust the upper limit to accommodate labels
+        ylim(-fixed_max_radius * 0.4, fixed_max_radius * 1.2) +  # Adjust the upper limit to accommodate labels
         theme_minimal() +
         theme(
           legend.position = c(0.51, 0.48),
           legend.justification = c(0.5, 0.2),
           legend.title = element_blank(),
-          legend.text = element_text(size = 14),
+          legend.text = element_text(size = 18),
           axis.text = element_blank(),
           axis.title = element_blank(),
           panel.grid = element_blank(),
@@ -305,22 +309,22 @@ cohort_exts <- c("", "_no_triple")
           aes(x = label_x, y = label_y, label = description, hjust = hjust),
           color = "black",
           alpha = 0.8,
-          size = 4.5,  # Reduced size for better fit
+          size = 6.5,  # Reduced size for better fit
           angle = label_data$angle,
           inherit.aes = FALSE
         ) +
         geom_text(
           data = label_percentages,
-          aes(x = label_x, y = -fixed_max_radius *0.12 , label = sprintf("%.1f%%", prev), hjust = hjust),
+          aes(x = label_x, y = -fixed_max_radius *0.13 , label = sprintf("%.1f%%", prev), hjust = hjust),
           color = "black", 
           alpha = 0.8,
-          size = 4.5,  # Reduced size for better fit
+          size = 5.2,  # Reduced size for better fit
           angle = label_percentages$angle,
           inherit.aes = FALSE
         )
       
       print(p)
-
+      
       # Save
       file_path <- file.path(
         HDPS_folder,
@@ -328,7 +332,7 @@ cohort_exts <- c("", "_no_triple")
         "diagnostics",
         paste0("conceptsPlot_top", k, outcome, cohort_ext, ".png")
       )
-      ggsave(file_path, p, width = 16, height = 16)
+      ggsave(file_path, p, width = 16, height = 16, bg = "white")
       
     }
   }
